8c39357a1212c2bee414c87cdb0866d5aaf52996
Simon O'Hara <simonpohara@gmail.com>
2017-09-04 06:30:58 +1000

    added react-hexgrid and built a static replica of the visual concept that includes non-functional interaction and animated transition to flipped hexagon states

--- src/App.js
+++ src/App.js
@@ -1,18 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { HexGrid, Layout, Hexagon, Text } from 'react-hexgrid';
 import './App.css';
 
+const hexagons = [
+  { q:-1,r:0,s:0, source: 'foo' },
+  { q:-1,r:1,s:0 },
+
+  { q:0,r:-1,s:0 },
+  { q:0,r:0,s:0 },
+  { q:0,r:1,s:0 },
+
+  { q:1,r:-1,s:0 },
+  { q:1,r:0,s:0, source: 'bar' },
+
+  { q:2,r:-1,s:0 },
+  { q:2,r:0,s:0 }
+]
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      destinations: true
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    const ev = Object.assign({}, e);
+    this.setState(prevState => ({
+      destinations: !prevState.destinations
+    }));
+    window.ev = ev;
+    console.log(ev.target);
+  }
+
   render() {
+    const { destinations } = this.state;
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Basic example of HexGrid usage.</h1>
+        <HexGrid width={1200} height={825}>
+          <Layout className={destinations ? "show-destinations" : "hide-destinations"} size={{ x:10, y:10 }} origin={{ x: -7, y: 0 }} spacing={1.2}>
+            {
+              hexagons.map((hex, i) => {
+                const
+                  classes = [ "destination" ],
+                  text = `${hex.q}, ${hex.r}, ${hex.s}`;
+                hex.source && classes.push("has-source");
+                return (
+                  <Hexagon className={classes.join(' ')} key={i} q={hex.q} r={hex.r} s={hex.s} onClick={this.handleClick}>
+                    <Text>{text}</Text>
+                  </Hexagon>
+                );
+              })
+            }
+          </Layout>
+        </HexGrid>
       </div>
     );
   }
